4284d8f2801867caad61d81ebf89c0b492988c09
Thom Wickes <thomas.wickes@disneyanimation.com>
2015-10-02 17:28:50 -0700

    added iResolution

--- 1_pixelshader_basic/shaders/base.fs
+++ 1_pixelshader_basic/shaders/base.fs
@@ -3,8 +3,10 @@ precision highp float;
 #endif
 #extension GL_OES_standard_derivatives : enable
 uniform float     iGlobalTime;
-
+uniform vec3	  iResolution;
 
 void main(void) {
-   gl_FragColor = vec4(abs(sin(iGlobalTime * 10.0)), 0.2, 0.3, 1.0);
+   /*gl_FragColor = vec4(abs(sin(iGlobalTime * 10.0)), 0.2, 0.3, 1.0);*/
+	gl_FragColor = vec4(gl_FragCoord.x/iResolution.x, gl_FragCoord.y/iResolution.y, 0.0, 1.0);
+
 }
