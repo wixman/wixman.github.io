0a292e9bc046d8ce00927124a773b0660bd8ed51
Thom Wickes <thomas.wickes@disneyanimation.com>
2015-10-05 18:47:08 -0700

    keyboard input!

--- 1_pixelshader_basic/shaders/base.fs
+++ 1_pixelshader_basic/shaders/base.fs
@@ -6,7 +6,9 @@ uniform float     iGlobalTime;
 uniform vec3	  iResolution;
 
 void main(void) {
-   /*gl_FragColor = vec4(abs(sin(iGlobalTime * 10.0)), 0.2, 0.3, 1.0);*/
+
+	
+	
 	gl_FragColor = vec4(gl_FragCoord.x/iResolution.x, gl_FragCoord.y/iResolution.y, 0.0, 1.0);
 
 }
