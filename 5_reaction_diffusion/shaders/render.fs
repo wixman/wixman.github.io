764195cc6469635fc1537cf757a521b1343b81fc
Thom Wickes <thomas.wickes@disneyanimation.com>
2016-06-10 15:38:41 -0700

    from script

--- 5_reaction_diffusion/shaders/render.fs
+++ 5_reaction_diffusion/shaders/render.fs
@@ -24,57 +24,26 @@ void main()
 	/*float value = texture2D(u_texture, vUv).g;*/
 	float value = texture2D(u_texture, vUv).g;
 
-
-	// get the gradient
-	float sample_x1 = texture2D(u_texture, vUv+vec2(step_x, 0.0)).g;
-	float sample_y1 = texture2D(u_texture, vUv+vec2(0.0, step_y)).g;
-	float sample_x2 = texture2D(u_texture, vUv+vec2(step_x * 2.0, 0.0)).g;
-	float sample_y2 = texture2D(u_texture, vUv+vec2(0.0, step_y* 2.0)).g;
-	float sample_x3 = texture2D(u_texture, vUv+vec2(step_x * 2.0, 0.0)).g;
-	float sample_y3 = texture2D(u_texture, vUv+vec2(0.0, step_y* 2.0)).g;
-
-    
     //Normals
-    
-    float dx = dFdx(value*2.0)*u_resolution.x/80.0*value*5.0;
-    float dy = dFdy(value*2.0)*u_resolution.x/80.0*value *5.0;
-     
-    vec3 vNormal = normalize(vec3(dx,dy,sqrt(clamp(1.0-dx*dx-dy*dy,0.0,1.0))));
-    
-
-	float sample_x = (sample_x1 + sample_x2 + sample_x3) / 3.0;
-	float sample_y = (sample_y1 + sample_y2 + sample_y3) / 3.0;
-	vec2 grad = normalize(vec2(value-sample_x1, value - sample_y1));
+    float dx = dFdx(value*2.0)*u_resolution.x/80.0*value*10.0;
+    float dy = dFdy(value*2.0)*u_resolution.x/80.0*value *10.0;
+    vec3 normal = normalize(vec3(dx,dy,sqrt(clamp(1.0-dx*dx-dy*dy,0.0,1.0))));
 	
-	value = fit(value, 0.0, 0.4, 0.0, 1.0);
-	vec3 col2 = mix(vec3(0.9, 0.9, 0.9), vec3(0.2, 0.2, 0.3), pow(value,2.0)*2.);
-	/*vec3 col = mix(u_bgcolor, u_mcolor, value*2.5);*/
-	/*col = mix(col, col2, u_source.x/u_resolution.x);*/
-	/*col = vec3(vUv, 1.0);*/
+	/*value = fit(value, 0.0, 0.4, 0.0, 1.0);*/
 
+	/*vec3 col2 = mix(vec3(0.9, 0.9, 0.9), vec3(0.2, 0.2, 0.3), pow(value,2.0)*2.);*/
 
-	// sample the gradient
+	vec3 bgcolor = vec3(0.3,0.3,0.3);
 
+    vec3 light = normalize(vec3(u_resolution.xy-u_source.xy,48.0)-vec3(u_resolution.xy/2.0,0.0));
+    vec3 i = vec3(0.0,0.0,1.0);
 
-	// perterb normal with gradient
+    float shading = clamp(dot(normal,light),0.0,1.0);
 
-	// light light light!
+    vec3 spec = pow(clamp(dot(light,reflect(vec3(0.0,0.0,-1.0),normal)),0.0,1.0),25.0)*vec3(0.5,0.5,0.6);
 
-	
-	// ok lets try and make a normal map	
-	/*col = vec3(grad, 1.0) * value;*/
-	/*if(vUv.x < 0.5)*/
-		/*col = vec3(sample_y1);*/
-	/*else*/
-		/*col = vec3(value);*/
+    vec3 finalcol = bgcolor+vec3(0.4,0.4,0.35)*shading+spec*value;
 
-	/*grad.x = max(grad.x, 0.0);*/
-	/*vec3 spec = vec3(grad.x, grad.x, grad.x)* 0.01;*/
-	/*col2 += vec3(spec);*/
-
-	/*value = clamp(value, 0.0, 1.0);*/
-
-	gl_FragColor = vec4(vNormal, 1.0);
-	/*gl_FragColor = vec4(grad.x) * value;*/
+	gl_FragColor = vec4(finalcol, 1.0);
 
 }
