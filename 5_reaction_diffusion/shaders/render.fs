4f7d9ce61a16e2642faa11684e02f5b17b534181
Thom Wickes <thomas.wickes@disneyanimation.com>
2016-06-10 12:21:34 -0700

    from script

--- 5_reaction_diffusion/shaders/render.fs
+++ 5_reaction_diffusion/shaders/render.fs
@@ -13,7 +13,7 @@ void main()
 	float value = texture2D(u_texture, vUv).g;
 	vec3 col;
 	vec3 col2 = mix(vec3(0.1, 0.1, 0.4), vec3(1.0, 0.2, 0.2), value*2.);
-	col = mix(u_bgcolor, u_mcolor, value*2.);
+	col = mix(u_bgcolor, u_mcolor, value*2.5);
 	/*col = mix(col, col2, u_source.x/u_resolution.x);*/
 	/*col = vec3(vUv, 1.0);*/
 	gl_FragColor = vec4(col, 1.0);
